30ef26328acc0bd7b95e5baf33ad35e599993a0d
pi <pi@brokenrul.es>
2015-04-09 13:08:43 +0200

    Cleanup

--- _site/create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cp -r css ../vertical-progress/_site
-cp -r img ../vertical-progress/_site
-cp -r javascript ../vertical-progress/_site
-gollum-site --output_path ../vertical-progress/_site --working generate
-perl toc.pl > ../vertical-progress/_site/Ingredients.html
